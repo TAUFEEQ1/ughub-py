9c5a28324c7b15f8b66e2ae135b44378571ad968
TAUFEEQ1 <TAUFEEQ1@users.noreply.github.com>
2023-07-05 20:26:22 +0000

    ✅ Ready to clone and code.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from ughub_py.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "ughub_py"
---
+++ ughub_py/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for ughub_py."""
+
+from ughub_py.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
